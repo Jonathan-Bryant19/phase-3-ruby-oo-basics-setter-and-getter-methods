5bbfde18dc4a9098c6250af95440e5404f9e254f
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-23 13:50:45 -0800

    commit tree cdb0ccefd1c7e9b4c291426bacab4ff13815b027

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    
+    def name=(value)
+        @name = value
+    end
+
+    def breed=(value)
+        @breed = value
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 
 end
